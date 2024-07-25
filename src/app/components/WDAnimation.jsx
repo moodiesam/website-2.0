ed21d5360da6cfff7d3938ed035b29fe7c9f55c6
moodiesam <sampmcampbell@gmail.com>
2024-07-25 14:08:39 -0400

    isolate animation of homepage and correct logit to run each time

---
+++ src/app/components/WDAnimation.jsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useEffect } from "react"
+import { useSpring, animated } from '@react-spring/web'
+
+const WDAnimation = () => {
+
+    const [springs, api] = useSpring(() => (
+        {
+          from: {x: 0, opacity: 0, scale: 0.5, rotate: "180deg"}
+        }
+      ))
+    
+      useEffect(() => {
+        api.start({
+          to: {x: 400},
+        })
+        api.start({
+          to: {opacity: 1},
+        })
+        api.start({
+          to: {scale: 1},
+        })
+        // api.start({
+        //   to: {rotate: "0deg"},
+        // })
+      })
+
+      const handleSpin = () => {
+        api.start({
+            to: {
+                rotate: "0deg"
+            }
+        })
+      }
+    
+    return (
+        <animated.p onMouseEnter={handleSpin} style={{...springs}} className="text-3xl pt-10">Web Developer</animated.p>
+    )
+}
+
+export default WDAnimation
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,15 @@
 'use client'
 
 import "./globals.css"
-import { useSpring, animated } from '@react-spring/web'
+import WDAnimation from './components/WDAnimation'
 
 export default function Home() {
-
-  const spring = useSpring({
-    from: {x: 0, opacity: 0, scale: 0.5, rotate: "180deg"},
-    to: {x: 400, opacity: 1, scale: 1, rotate: "0deg"},
-  })
-
-  console.log('hello')
 
   return <>
     <section id="homepage">
       <div className="container flex flex-col align-center items-center px-4 mx-auto space-y-12 md:space-y-0">
         <h1 className="max-w-4xl text-4xl font-bold text-center mt-36 md:text-9xl">Sam Campbell</h1>
-        <animated.p style={{...spring}} className="text-3xl pt-10">Web Developer</animated.p>
+        <WDAnimation />
       </div>
     </section>
   </>
